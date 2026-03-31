50518aab691f47e15db4eb1b3518c6f2dfcbc844
unknown <pranaypatkar0607@gmail.com>
2026-03-31 13:11:59 +0530

    fix: hardcode supabase keys directly in module to resolve Vercel 404 deployment error

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,7 @@
 // js/supabase.js
-import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+const SUPABASE_URL = 'https://ksazgkpfziklmzhnwyji.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzYXpna3BmemlrbG16aG53eWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5Mjk2MDgsImV4cCI6MjA5MDUwNTYwOH0.bNjorLpUmU5wlgSNh33y_RxrcLxQkWuUuJ_neldcJYA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
